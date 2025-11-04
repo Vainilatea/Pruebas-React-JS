23c46de2a46f07c4297d38bb49c11c78e6ce691b
Vainilatea <124099125+Vainilatea@users.noreply.github.com>
2025-11-04 14:00:15 +0100

    Add files via upload

---
+++ pruebas.js
@@ -0,0 +1,20 @@
+const enemy1 = { //const es el objeto
+name: "feldespato", //propiedad de objeto
+level: 3, //propiedad de obeto -> clave: valor
+skills: [ "fuego blanco", "llorar desesperadamente"]
+}
+
+console.log (enemy1.skills)
+
+///////////////
+
+function sum (numbers)
+let result = 0
+
+let i = 0
+while (i < number1.lenght) {
+    result = result + numbers [i]
+
+    i++
+}
+?
