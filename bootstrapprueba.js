3690c6e9e7ee4d22cc44648ceb811594ee1627c4
Vainilatea <124099125+Vainilatea@users.noreply.github.com>
2025-11-07 14:05:40 +0100

    Update bootstrapprueba.js

--- bootstrapprueba.js
+++ bootstrapprueba.js
@@ -1,1 +1,9 @@
 bootstrapping
+
+ const [darkMode, setDarkMode] = useState (false)
+
+<div className= {darkMode === true ? "dark-mode" : "light-mode" }>
+rederiizado condicional
+funciones flecha
+className="imagesize"
+className="flextabla"
