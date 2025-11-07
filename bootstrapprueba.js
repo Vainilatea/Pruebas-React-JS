9f63968e57b72940cc8749d3e9065f2d0996913e
Vainilatea <124099125+Vainilatea@users.noreply.github.com>
2025-11-07 14:06:15 +0100

    Update bootstrapprueba.js

--- bootstrapprueba.js
+++ bootstrapprueba.js
@@ -7,3 +7,16 @@ rederiizado condicional
 funciones flecha
 className="imagesize"
 className="flextabla"
+
+
+objetos: estructura de datos formada por parejas clave:valor
+bootstrapping -> vite --------------- npm run dev
+
+desarrollo projecto react
+1) crear proyecto from scratch
+	 npm create vite@latest NOMBRE -- --template react-ts
+2) donde puedo tocar:
+ src/ 
+	App.jsx
+	App.css
+3)
